f29af436b349631386f79fec9d2cd4a61d1ecc95
ikuradou745-oss <ikuradou745@gmail.com>
2026-02-22 11:09:29 +0900

    Create firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBXnNXQ5khcR0EvRide4C0PjshJZpSF4oM",
+    authDomain: "typing-game-28ed0.firebaseapp.com",
+    databaseURL: "https://typing-game-28ed0-default-rtdb.firebaseio.com",
+    projectId: "typing-game-28ed0",
+    storageBucket: "typing-game-28ed0.firebasestorage.app",
+    messagingSenderId: "963797267101",
+    appId: "1:963797267101:web:0d5d700458fb1991021a74",
+    measurementId: "G-CL4B6ZK0SC"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
